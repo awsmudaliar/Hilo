d3f5b0017abee938a8192ee9b69f9850a5e0c1db
awsmudaliar <awsmudaliar@outlook.com>
2022-05-13 16:03:17 -0600

    Update 2

--- game/die.py
+++ game/die.py
@@ -4,20 +4,11 @@ class Die:
     def __init__(self):
         self.value = 0
         self.points = 0
-        self.hilorequest = ""
-        self.previous_value = 0
+        #self.hilorequest = ""
+        #self.previous_value = 0
 
 
-    def roll(self):
+    def roll():
         #Card values range from 1 - 13
-        self.value = random.randint(1,13)
-        if self.hilorequest == "h" or "H":
-            if self.value > self.previous_value:
-                self.points = 100
-            else:
-                self.points = -75
-        elif self.hilorequest == "l" or "L":
-            if self.value < self.previous_value:
-                self.points = 100
-            else:
-                self.points = -75
+        value = random.randint(1,13)
+        return value
--- game/director.py
+++ game/director.py
@@ -4,19 +4,18 @@ import random
 class Director:
 
     def __init__(self):
-
-        self.dice = []
+        self.dice = 0
         self.is_playing = True
+        self.value = 0
         #Player starts with 300 points
         self.score = 0
         self.total_score = 300
         self.hilorequest = ""
         self.previous_value = 0
-        die = Die()
-        self.dice.append(die)
 
     def start_game(self):
-        self.previous_value = random.randint(1,13)
+        #self.previous_value = random.randint(1,13)
+        self.previous_value = Die.roll()
 
         print ("Welcome to Hi/Lo")
         print()
@@ -43,10 +42,20 @@ class Director:
         if not self.is_playing:
             return 
 
-        die = self.dice[0]
-        die.roll()
-        self.score += die.points 
-        self.total_score += self.score
+        self.value = Die.roll()
+
+        # Evaluate the card
+        if self.hilorequest == "h" or "H":
+            if self.value > self.previous_value:
+                self.points = 100
+            else:
+                self.points = -75
+        elif self.hilorequest == "l" or "L":
+            if self.value < self.previous_value:
+                self.points = 100
+            else:
+                self.points = -75
+
 
     def do_outputs(self):
 
