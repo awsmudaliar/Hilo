c9cfb3e6023868961bd7480b761584eadb9d870d
awsmudaliar <awsmudaliar@outlook.com>
2022-05-13 15:30:02 -0600

    Update#1

--- game/die.py
+++ game/die.py
@@ -1,11 +1,23 @@
 import random
+
 class Die:
     def __init__(self):
         self.value = 0
         self.points = 0
+        self.hilorequest = ""
+        self.previous_value = 0
+
 
     def roll(self):
-        self.value = random.randint(1,6)
-        self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
-        
-            
+        #Card values range from 1 - 13
+        self.value = random.randint(1,13)
+        if self.hilorequest == "h" or "H":
+            if self.value > self.previous_value:
+                self.points = 100
+            else:
+                self.points = -75
+        elif self.hilorequest == "l" or "L":
+            if self.value < self.previous_value:
+                self.points = 100
+            else:
+                self.points = -75
--- game/director.py
+++ game/director.py
@@ -1,5 +1,5 @@
 from game.die import Die
-
+import random
 
 class Director:
 
@@ -7,15 +7,18 @@ class Director:
 
         self.dice = []
         self.is_playing = True
+        #Player starts with 300 points
         self.score = 0
-        self.total_score = 0
-
+        self.total_score = 300
+        self.hilorequest = ""
+        self.previous_value = 0
         die = Die()
         self.dice.append(die)
 
     def start_game(self):
+        self.previous_value = random.randint(1,13)
 
-        print ("Welcome to Dice")
+        print ("Welcome to Hi/Lo")
         print()
         input("Press Enter to Start...") 
 
@@ -30,17 +33,9 @@ class Director:
         print(f"Score: {self.total_score}\n")
 
     def get_inputs(self):
-        while True:
-            roll_dice = input("Roll dice? [y/n] ")
-            if roll_dice == "y":
-                break
-            elif roll_dice == "n":
-                break
-            else:
-                print("Invalid Input")
-                print("")
-
-        self.is_playing = (roll_dice == "y")
+        # Get input for hi/lo
+        print(f"Current Value is: {self.previous_value}")
+        self.hilorequest = input("Higher or Lower (H/L): ")
 
        
     def do_updates(self):
@@ -48,10 +43,9 @@ class Director:
         if not self.is_playing:
             return 
 
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            die.roll()
-            self.score += die.points 
+        die = self.dice[0]
+        die.roll()
+        self.score += die.points 
         self.total_score += self.score
 
     def do_outputs(self):
@@ -60,10 +54,9 @@ class Director:
             return
         
         values = ""
-        for i in range(len(self.dice)):
-            die = self.dice[i]
-            values += f"{die.value} "
+        self.score = 0
+        self.value = self.previous_value
 
-        print(f"You rolled: {values}")
+        print(f"You rolled: {self.value}")
         print(f"Your score is: {self.total_score}\n")
         self.is_playing == (self.score > 0)
